5dcd0c14b657c3fbea6f9ad29e84ad6bc7e569b6
XI2423-RahulSharma <84383730+xi2423-rahulsharma@users.noreply.github.com>
2023-01-24 20:07:38 +0530

    drawer translation

--- src/common/DrawerSidePannel.tsx
+++ src/common/DrawerSidePannel.tsx
@@ -4,6 +4,12 @@ import AppContext from '../AppContext';
 const DrawerSidePannel = (props: any) => {
   const { setNavbar, navbar } = props;
   const value = useContext(AppContext);
+
+
+  const {
+    drawerSideOption
+  }: any = value?.state?.languages;
+
   const { locale } = value.state;
 
   const lang = '';
@@ -42,66 +48,53 @@ const DrawerSidePannel = (props: any) => {
       <div className=" h-screen bg-salam-blue text-white overflow-y-scroll  hide-scrollbar z-100 text-xl">
         <div className="px-8 py-16 flex-1">
           <div className=" pt-12 ">
-            <div className=" text-accent text-4xl flex">Fiber</div>
+            <div className=" text-accent text-4xl flex">{drawerSideOption?.fiber}</div>
             <div className=" pl-8 pt-8  pb-16 font-bold flex flex-col gap-6">
               <div>
-                <a
-                  href={`https://salam.sa/${lang}/personal/fiber-prepaid`}
-                  className="z-200"
-                >
-                  Prepaid
+                <a href={`https://salam.sa/${lang}/personal/fiber-prepaid`} className="z-200">
+                  {drawerSideOption?.prepaid}
                 </a>
               </div>
               <div>
-                <a
-                  href={`https://salam.sa/${lang}/personal/fiber-postpaid`}
-                  className="z-200"
-                >
-                  Postpaid
+                <a href={`https://salam.sa/${lang}/personal/fiber-postpaid`} className="z-200">
+                  {drawerSideOption?.postpaid}
                 </a>
               </div>
               <div>
-                <a
-                  href={`https://salam.sa/${lang}/personal/gaming`}
-                  className="z-200"
-                >
-                  Gamers pack
+                <a href={`https://salam.sa/${lang}/personal/gaming`} className="z-200">
+                  {drawerSideOption?.gamers_pack}
                 </a>
               </div>
               <div>
                 <a href={`https://salam.sa/${lang}/personal/orbit-family-pack`}>
-                  Orbit Family Pack
+                  {drawerSideOption?.orbit_family_pack}
                 </a>
               </div>
             </div>
-            <div className=" text-accent text-4xl flex">5G</div>
+            <div className=" text-accent text-4xl flex">{drawerSideOption?._5_g}</div>
             <div className="  pl-8 pt-8  pb-16 font-bold flex flex-col gap-6">
               <div>
-                <a href={`https://salam.sa/${lang}/personal/orbit-family-pack`}>
-                  Salam 5G
-                </a>
+                <a href={`https://salam.sa/${lang}/personal/orbit-family-pack`}>{drawerSideOption?.salam_5g}</a>
               </div>
               <div>
                 <a href={`https://salam.sa/${lang}/personal/5g-coverage-check`}>
-                  5G Check coverage
+                  {drawerSideOption?._5g_check_coverage}
                 </a>
               </div>
             </div>
             <div className=" h-[1px] divide-y-4 bg-dimWhite opacity-10 " />
           </div>
           <div className=" flex-none text-xl flex flex-col gap-6 font-thin py-8">
-            <div className=" font-medium ">Other</div>
-            <div>Login to My Account</div>
+            <div className=" font-medium ">{drawerSideOption?.other}</div>
+            <div>{drawerSideOption?.login_to_my_account}</div>
             <div>
-              <a href={`https://salam.sa/${lang}/slm/about`}>About Salam</a>
+              <a href={`https://salam.sa/${lang}/slm/about`}>{drawerSideOption?.about_salam}</a>
             </div>
             <div>
-              <a href={`https://salam.sa/${lang}/support/complaints`}>
-                Complaints
-              </a>
+              <a href={`https://salam.sa/${lang}/support/complaints`}>{drawerSideOption?.complaints}</a>
             </div>
             <div>
-              <a href={`https://salam.sa/${lang}/personal`}>Contact Us</a>
+              <a href={`https://salam.sa/${lang}/personal`}>{drawerSideOption?.contact_us}</a>
             </div>
           </div>
         </div>
