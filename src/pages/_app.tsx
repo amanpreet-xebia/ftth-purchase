aa7fef23ab03cb8990eaf76413163a8d4309ac96
Amanpreet-xebia <amanpreet.singh@xebia.com>
2023-01-31 12:14:29 +0530

    code refactoring and removing 404 on entrypoint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,21 @@
 'use client';
-// import '@/styles/globals.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../common/Navbar';
 import Footer from '../common/Footer';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppContext from '../AppContext';
 import { translations } from '../constants';
-// import Sidebar from './components/Sidebar';
-import { ToastContainer, toast } from 'react-toastify';
 import AuthTokenProvider from '../context/alertContext/AlertContext';
 import AuthToken from '../context/AuthToken';
 
 import Spinner from '../components/common/spinner/spinner';
-import Head from 'next/head';
-import Images from '../components/images';
 export default function App({ Component, pageProps }: AppProps) {
   const storedLang: string =
     typeof window !== 'undefined'
       ? localStorage.getItem('selectedLanguage') || 'en'
       : 'en';
   const [locale, setLocale] = useState(storedLang);
-  const [user, setUser] = useState({});
   const translation: any = translations;
 
   const [showing, setShowing] = useState(false);
@@ -33,19 +27,6 @@ export default function App({ Component, pageProps }: AppProps) {
   if (!showing) {
     return null;
   }
-  // const userData: string = sessionStorage.getItem('UserData') || '';
-  // setUser(userData);
-  // useEffect(() => {
-  //   if (Object.keys(userData).length !== 0){
-  //   debugger
-  //   // console.log(contextData.state.user);
-  //   setUser(userData);
-  //   }
-  // }, [userData])
-
-  // if (Object.keys(user).length === 0){
-  //   router.push('/login')
-  // }
   return (
     <>
       <AuthTokenProvider>
@@ -55,10 +36,8 @@ export default function App({ Component, pageProps }: AppProps) {
               state: {
                 languages: translation[locale],
                 locale: locale,
-                // user: JSON.parse(userData) || {},
               },
               setLocale: setLocale,
-              setUser: setUser,
             }}
           >
             <div
@@ -69,12 +48,8 @@ export default function App({ Component, pageProps }: AppProps) {
               <Navbar></Navbar>
               <div className="my-20 md:my-0 md:h-[calc(100vh-284px)] lg:min-h-[800px] md:h-full pb-4">
                 <main>
-                  {/* <Alert/> */}
-                  {/* <ToastContainer /> */}
                   <Component {...pageProps} />
                 </main>
-
-                {/* <button onClick={showToastMessage}>Notify</button> */}
               </div>
 
               <Footer />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
-// //import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css';
-
-// const inter = Inter({ subsets: ['latin'] })
+import { useContext, useEffect } from "react";
+import AppContext from '../AppContext';
 
 export default function Home() {
+const value = useContext(AppContext);
+const { locale } = value.state;
+  useEffect(() => {
+    window.location.href = `https://salam.sa/${locale || 'en'}/personal`;
+  }, []);
   return <></>;
 }
