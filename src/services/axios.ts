9a7a69e352fa55e4754b51c6d1f445a1ebb33d72
prince.singh <prince.singh@xebia.com>
2023-02-01 12:54:57 +0530

    environment variable change

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
-const token = typeof window !== 'undefined' ? localStorage.getItem('token') : '';
-const storedLang = typeof window !== 'undefined' ? localStorage.getItem('selectedLanguage') : 'en'
+const token =
+  typeof window !== 'undefined' ? localStorage.getItem('token') : '';
+const storedLang =
+  typeof window !== 'undefined'
+    ? localStorage.getItem('selectedLanguage')
+    : 'en';
 
 export default axios.create({
-   //baseURL: `${process.env.API_BASE_LOCAL_URL}`,
-   baseURL: 'http://165.227.158.208/api',
+  //baseURL: `${process.env.API_BASE_LOCAL_URL}`,
+  baseURL: `${process.env.API_BASE_PROD_URL}`,
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Credentials': 'true',
@@ -12,6 +16,6 @@ export default axios.create({
     accept: 'application/json',
     prefer: '200',
     Authorization: token ? `Bearer ${token}` : '',
-    'Accept-language': storedLang
-  }
+    'Accept-language': storedLang,
+  },
 });
